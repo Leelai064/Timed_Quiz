8548bd4848a161a83bf5bd7a3a31b9885e7a9ca1
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-05-07 05:56:02 -0400

    Onclick Timer countdown completed

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-submitbtn.onclick = HighscoreTally;
-startbtn.onclick = BeginQuiz;
-
-//listing my variables
-
-var question= document.querySelector("#questions");
-var timer= document.querySelector("#time");
-var choice =document.querySelector("#choice");
-var startbtn= document.querySelector("#begin");
-var submitbtn = document.querySelector("#done");
-var results = document.querySelector("results");
-
-//Timer and question queue variables
-var timerId;
-var time = question.length * 20;
-var CurrentQue= 0;
-
-//event listener: when the button is clicked the timer starts.
-
+//Here is my event listener. When the Begin button is clicked, the timer starts.
 document.getElementById("Startbtn").addEventListener("click", timer);
 // Begin Quiz function
 
-function beginquiz(){
-    //I want a start screen that disappears once the questions start rolling out. Here's the variable for that
-    document.getElementById
-  
-
+function begin(){
+    var counter= 75;
+    setInterval(function(){
+        counter--;
+        if(counter >= 0){
+            span = document.getElementById("time_countdown");
+            span.innerHTML = counter;
+        }
+        if (counter === 0){
+            alert('Oops! Look like you ran out of time!');
+            clearInterval(counter);
+        }
+    }, 1000);
 }
+function beginquiz(){
+    document.getElementById("count").style="color:black;";
+    timecountdwn();
+};
+//I want a start screen that disappears once the questions start rolling out. Here's the variable for that
