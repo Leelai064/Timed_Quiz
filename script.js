17f1db643fab1e6273d794f1d92fa36dae2c4c0f
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-08-21 03:42:14 -0400

    Added a finalScores file

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 //Here is my event listener. When the Begin button is clicked, the timer starts.
-document.getElementById("start_btn").addEventListener("click", begin);
+document.getElementById("beginBtn").addEventListener("click", begin);
+
 //buttons needed
-var nxtquestion = document.querySelector("#nxtquestion"); //next question
+var nxtQuestion = document.querySelector("#nxtQuestion"); //next question
 
 // Question Elements
+function displayQs(){
+    choiceCreation.innerHTML="";
+ 
+        var askedQ = questions[questionNum].title;
+        console.log(askedQ);
+        var givchoices= questions[questionNum].choices;
+        console.log(givchoices);
+        // questions.textContent=askedQ;
+        
+        //give choicebtns a class
+        // look up the difference between innertext and textconent
+        givchoices.forEach(function(newItem){
+            console.log(newItem);
+            var displayItem=document.createElement("button");
+            displayItem.classList.add("choiceBtn");
+            displayItem.textContent = newItem;
+            displayItem.addEventListener("click",(correctAnswer));
+        //     // questionsList.appendChild(choiceCreation);
+           questionsList.appendChild(displayItem);
+        })
+    // }
+    //Need to have this for loop cycle for each question (Work on)
+}
 function begin(){
+    console.log("Button works");
     displayQs()
     var counter= 75;
     setInterval(function(){
         counter--;
         if(counter >= 0){
-            span = document.getElementById("time_countdown");
+            span = document.getElementById("timeCountdown");
             span.innerHTML = 'Time Left: ' + counter;
         }
         if (counter === 0){
@@ -22,70 +47,60 @@ function begin(){
 //Now we want to display these questions to the user, have them disappear when answered and checked for correct scores
 
 //Declaring useful variables
-var nxtquestion = document.querySelector("#nxtquestion"); //next question
-var qsarray = document.querySelector("#qsarray"); //questions array
+var nxtQuestion = document.querySelector("#nxtQuestion"); //next question
+var qsArray = document.querySelector("#qsArray"); //questions array
 var container = document.querySelector("#container");
-var questionnum=0;
-var choicecreation= document.createElement("p");
+var questionNum=0;
+var choiceCreation= document.createElement('button');
 
 
-var questionlist= document.querySelector("#questionslist"); //number of questions given
-var finalscore= 0; //Final score the user got
+var questionsList= document.querySelector("#questionsList"); //number of questions given
+var finalScore= 0; //Final score the user got
 var highScore=0;
 
 var scoreResult;
-var answer_checkpoint;
+var answerCheckpoint;
 
 //Element renewer
 var renew=document.createElement("renew");
 
-//submittion buttons for each possible choice.
-// var choice1btn= document.getElementById(btn0);
-// var choice2btn= document.getElementById(btn1);
-// var choice3btn= document.getElementById(btn2);
-// var choice4btn= document.getElementById(btn3);
-//Event listners for the choices and next question cycling
-//Endgame initial btn,view highscores, go back, and clear highscore btn
-// choice1btn.addEventListener("click", choose1);
-// choice2btn.addEventListener("click", choose2);
-// choice3btn.addEventListener("click", choose3);
-// choice4btn.addEventListener("click", choose4);
-
 //Eventually we will need a goback, clear, and view highscores button below
 
 //function for going through the questions created above
-function displayQs(){
-    questionlist.innerHTML="";
-    renew.innerHTML="";
-    choicecreation.innerHTML="";
-    //Cyles through our questions array
-    for(var i=0; i< questions.length; i++){
-        var askedq = questions[questionnum].title;
-        var givchoices= questions[questionnum].choices;
-        questionlist.textContent=askedq;
+// function displayQs(){
+//     choiceCreation.innerHTML="";
+ 
+//         var askedQ = questions[questionNum].title;
+//         console.log(askedQ);
+//         var givchoices= questions[questionNum].choices;
+//         console.log(givchoices);
+//         // questions.textContent=askedQ;
         
-
-        givchoices.forEach(function(newItem){
-            var disItem=document.createElement("display");
-            disItem.textContent = newItem;
-            questionlist.appendChild(choicecreation);
-            choicecreation.appendChild(disItem);
-            disItem.addEventListener("click",correct_answer);
-        })
-    }
-    //Need to have this for loop cycle for each question (Work on)
-}
+//         //give choicebtns a class
+//         // look up the difference between innertext and textconent
+//         givchoices.forEach(function(newItem){
+//             console.log(newItem);
+//             var displayItem=document.createElement("button");
+//             displayItem.classList.add("choiceBtn");
+//             displayItem.textContent = newItem;
+//             displayItem.addEventListener("click",(correctAnswer));
+//         //     // questionsList.appendChild(choiceCreation);
+//            questionsList.appendChild(displayItem);
+//         })
+//     // }
+//     //Need to have this for loop cycle for each question (Work on)
+// }
 
 //So far the code does now know how to determine if the choice selected is the right answer
 
-function correct_answer(event){
+function correctAnswer(event){
     var element = event.target;
 
     if(element.matches("display")){
         var createnew = document.createElement("div");
         createnew.setAttribute("id","createnew");
 
-        if(element.textContent == questions[questionlist].answer){
+        if(element.textContent == questions[questionsList].answer){
             score ++;
             createnew.textContent= "Correct!";
         }
@@ -94,29 +109,31 @@ function correct_answer(event){
             createnew.textContent = "Wrong!"
         }
     }
-    questionlist ++
-    if (questionlist >= questions.length){
+    questionsList ++
+    if (questionsList >= questions.length){
         complete();
-        createnew.textContent = "End of Quiz!"+ "" + "You final score is: " + finalscore + "/" + questions.length;
+        createnew.textContent = "End of Quiz!"+ "" + "You final score is: " + finalScore + "/" + questions.length;
     }
     else{
-        displayQs(quizqs);
+        displayQs();
     }
-    qsarray.appendChild(createnew);
 }
-function choose1() {correct_answer(0);}
-function choose2() {correct_answer(1);}
-function choose3() {correct_answer(2);}
-function choose4() {correct_answer(3);}
+function choose1() {correctAnswer(0);}
+function choose2() {correctAnswer(1);}
+function choose3() {correctAnswer(2);}
+function choose4() {correctAnswer(3);}
 
+function quizcomplete(){
+    
+}
 //Save question answers to local storage function
-function storedchoices(){
+function storedChoices(){
     localStorage.setItem("scoreResults",JSON.stringify(scoreResult));
 }
 //Display final scores function
 
 //Clear all scores function
-function clean_slate(){
+function cleanSlate(){
     localStorage.clear();
     scoreResultsEl.innerHTML="";
 }
