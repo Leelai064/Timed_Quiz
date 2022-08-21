381dc538e452f08bea279661eef9364c0145c084
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-08-21 07:05:17 -0400

    questions.js file editeed (grammatical errors found)

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,26 +3,26 @@ var questions =[
     {
         title: "Commonly used data types DO NOT include:",
         choices: ["1. string","2. booleans","3. alerts","4. numbers"],
-        answer: "3"
+        answer: "3. alerts"
     },
     {
         title: "The condition in an if/else statement is enclosed within __________.",
         choices: ["1. quotes","2. curly brackets","3. parentheses","4. square brackets"],
-        answer: "3"
+        answer: "3. parentheses"
     },
     {
         title: "Sting values must be enclosed within ______.",
         choices: ["1. commas","2. curly brakcets","3. parentheses","4. quotes"],
-        answer: "4"
+        answer: "4. quotes"
     },
     {
         title: "Arrays in JavaScript can be used to store _______.",
-        choices: ["1. numbers and strings","2.. other arrays","booleans","all of the above"],
-        answer: "4"
+        choices: ["1. numbers and strings","2. other arrays","3. booleans","4. all of the above"],
+        answer: "4. all of the above"
     },
     {
         title: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["1. console.log","2. JavaScript","3. terminal/bash","4. for loops"],
-        answer: "1"
+        answer: "1. console.log"
     },
 ];
